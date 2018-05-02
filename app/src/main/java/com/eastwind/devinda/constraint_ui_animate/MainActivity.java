4ce22df2e57c6b5e722e13c83f44027c34a8a7f6
Janith Anoma <janith@elegantmedia.lk>
2018-05-02 15:32:59 +0530

    marge

--- app/src/main/java/com/eastwind/devinda/constraint_ui_animate/MainActivity.java
+++ app/src/main/java/com/eastwind/devinda/constraint_ui_animate/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.animation.AnticipateOvershootInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
